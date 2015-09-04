bb54336e4168634eaf93ced3f0d4ca2f205e85c4
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-09-05 01:44:08 +0200

    moved number of colors

--- Assets/scripts/GameProperties.cs
+++ Assets/scripts/GameProperties.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public  class GameProperties :MonoBehaviour {
-
-	private static float spawnRadius ;
+public  class GameProperties :MonoBehaviour
+{
+    public const int NUMBER_OF_COLORS = 8;
+    private static float spawnRadius ;
 	public static float SpawnRadius {get{return spawnRadius;}}
 	private static float noteDuration = 3 ;
 	public static float NoteDuration {get{return noteDuration;}}
 	public static float NoteVelocity {get{return SpawnRadius/ NoteDuration;}}
 	//private static float sunRadius = 
-	void Start(){
+	void Init(){
 		spawnRadius = Camera.main.orthographicSize * 0.8f;
 	}
 }
--- Assets/scripts/NoteSpawner.cs
+++ Assets/scripts/NoteSpawner.cs
@@ -3,16 +3,15 @@ using System.Collections;
 
 public class NoteSpawner : MonoBehaviour {
 
-	private const int NUMBER_OF_COLORS = 8;
 
-	private Vector3[] spawnpoints = new Vector3[NUMBER_OF_COLORS];
+	private Vector3[] spawnpoints = new Vector3[GameProperties.NUMBER_OF_COLORS];
 	public GameObject NotePrefab;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < NUMBER_OF_COLORS; i++) {
+		for (int i = 0; i < GameProperties. NUMBER_OF_COLORS; i++) {
 			spawnpoints[i] = new Vector3(
-					Mathf.Cos(2 * Mathf.PI/NUMBER_OF_COLORS * i) * GameProperties.SpawnRadius,
-				Mathf.Sin(2 * Mathf.PI/NUMBER_OF_COLORS * i) * GameProperties.SpawnRadius,
+					Mathf.Cos(2 * Mathf.PI/ GameProperties.NUMBER_OF_COLORS * i) * GameProperties.SpawnRadius,
+				Mathf.Sin(2 * Mathf.PI/ GameProperties.NUMBER_OF_COLORS * i) * GameProperties.SpawnRadius,
 					0
 				);
 			Debug.Log(spawnpoints[i].x);
