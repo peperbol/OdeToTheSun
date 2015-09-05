4e4ed706d06f8c9c8f2b03ea08fc266ac58cd5ce
Vincent Verhoeven <verhoevenv@gmail.com>
2015-09-05 12:50:54 +0200

    Refactor background pulsing to components

---
+++ Assets/scripts/BackgroundColorPulsar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class BackgroundColorPulsar : MonoBehaviour {
+
+    [Range(0, 4)]
+    public float maxColorPulse = 1.05f;
+
+    public void startPulse()
+    {
+        Camera target = GetComponent<Camera>();
+        StartCoroutine(pulseBackgroundColor(target, 0.1f, maxColorPulse, 0.5f));
+    }
+
+    private IEnumerator pulseBackgroundColor(Camera target, float widenTime, float maxBrightness, float shrinkTime)
+    {
+        float currTime = 0;
+        Color initial = target.backgroundColor;
+
+        while (currTime < widenTime && target != null)
+        {
+            float scale = Mathf.Lerp(1, maxBrightness, currTime / widenTime);
+            target.backgroundColor = initial * scale;
+
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+
+        while (currTime < shrinkTime && target != null)
+        {
+            float scale = Mathf.Lerp(maxBrightness, 1, (currTime - widenTime) / shrinkTime);
+            target.backgroundColor = initial * scale;
+
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+
+        if (target != null)
+        {
+            target.backgroundColor = initial;
+        }
+
+        yield return null;
+    }
+}
--- Assets/scripts/Juicificationator.cs
+++ Assets/scripts/Juicificationator.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System;
 
 public class Juicificationator : MonoBehaviour {
-    [Range(0, 4)]
-    public float maxColorPulse = 1.05f;
 
     public void onTheBeat()
     {
@@ -14,7 +12,11 @@ public class Juicificationator : MonoBehaviour {
 
     private void beatTheBackground()
     {
-        StartCoroutine(pulseBackgroundColor(Camera.main, 0.1f, maxColorPulse, 0.5f));
+        BackgroundColorPulsar[] pulsars = GameObject.FindObjectsOfType<BackgroundColorPulsar>();
+        foreach (BackgroundColorPulsar pulsee in pulsars)
+        {
+            pulsee.startPulse();
+        }
     }
 
     private void beatThePulsars()
@@ -27,35 +29,5 @@ public class Juicificationator : MonoBehaviour {
     }
 
 
-    private IEnumerator pulseBackgroundColor(Camera target, float widenTime, float maxBrightness, float shrinkTime)
-    {
-        float currTime = 0;
-        Color initial = target.backgroundColor;
-
-        while (currTime < widenTime && target != null)
-        {
-            float scale = Mathf.Lerp(1, maxBrightness, currTime / widenTime);
-            target.backgroundColor = initial * scale;
-
-            currTime += Time.deltaTime;
-            yield return null;
-        }
-
-        while (currTime < shrinkTime && target != null)
-        {
-            float scale = Mathf.Lerp(maxBrightness, 1, (currTime - widenTime) / shrinkTime);
-            target.backgroundColor = initial * scale;
-
-            currTime += Time.deltaTime;
-            yield return null;
-        }
-
-        if (target != null)
-        {
-            target.backgroundColor = initial;
-        }
-
-        yield return null;
-    }
 
 }
