8edb8b044bed8d597fc94210f66aa0a64fcdf374
Vincent Verhoeven <verhoevenv@gmail.com>
2015-09-05 12:42:28 +0200

    Refactor pulsing to a component

--- Assets/scripts/Juicificationator.cs
+++ Assets/scripts/Juicificationator.cs
@@ -3,15 +3,12 @@ using System.Collections;
 using System;
 
 public class Juicificationator : MonoBehaviour {
-    [Range(0,4)]
-    public float maxSizePulse = 1.25f;
-
     [Range(0, 4)]
     public float maxColorPulse = 1.05f;
 
     public void onTheBeat()
     {
-        beatTheNotes();
+        beatThePulsars();
         beatTheBackground();
     }
 
@@ -20,50 +17,15 @@ public class Juicificationator : MonoBehaviour {
         StartCoroutine(pulseBackgroundColor(Camera.main, 0.1f, maxColorPulse, 0.5f));
     }
 
-    private void beatTheNotes()
+    private void beatThePulsars()
     {
-        Note[] visibleNotes = GameObject.FindObjectsOfType<Note>();
-        foreach (Note note in visibleNotes)
+        SizePulsar[] pulsars = GameObject.FindObjectsOfType<SizePulsar>();
+        foreach (SizePulsar pulsee in pulsars)
         {
-            onTheBeat(note);
+            pulsee.startPulse();
         }
     }
 
-    private void onTheBeat(Note note)
-    {
-        StartCoroutine(pulseSize(note.transform, 0.02f, maxSizePulse, 0.5f));
-    }
-
-    private IEnumerator pulseSize(Transform target, float widenTime, float maxSize, float shrinkTime)
-    {
-        float currTime = 0;
-        Vector3 initialScale = target.localScale;
-
-        while (currTime < widenTime && target != null)
-        {
-            float scale = Mathf.Lerp(1, maxSize, currTime / widenTime);
-            target.localScale = initialScale * scale;
-
-            currTime += Time.deltaTime;
-            yield return null;
-        }
-
-        while (currTime < shrinkTime && target != null)
-        {
-            float scale = Mathf.Lerp(maxSize, 1, (currTime - widenTime) / shrinkTime);
-            target.localScale = initialScale * scale;
-
-            currTime += Time.deltaTime;
-            yield return null;
-        }
-
-        if ( target != null)
-        {
-            target.localScale = initialScale;
-        }
-
-        yield return null;
-    }
 
     private IEnumerator pulseBackgroundColor(Camera target, float widenTime, float maxBrightness, float shrinkTime)
     {
---
+++ Assets/scripts/SizePulsar.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Transform))]
+public class SizePulsar : MonoBehaviour {
+    [Range(0, 4)]
+    public float maxSizePulse = 1.25f;
+
+    public void startPulse()
+    {
+        Transform t = GetComponent<Transform>();
+        StartCoroutine(pulseSize(t, 0.02f, maxSizePulse, 0.5f));
+    }
+
+    private IEnumerator pulseSize(Transform target, float widenTime, float maxSize, float shrinkTime)
+    {
+        float currTime = 0;
+        Vector3 initialScale = target.localScale;
+
+        while (currTime < widenTime && target != null)
+        {
+            float scale = Mathf.Lerp(1, maxSize, currTime / widenTime);
+            target.localScale = initialScale * scale;
+
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+
+        while (currTime < shrinkTime && target != null)
+        {
+            float scale = Mathf.Lerp(maxSize, 1, (currTime - widenTime) / shrinkTime);
+            target.localScale = initialScale * scale;
+
+            currTime += Time.deltaTime;
+            yield return null;
+        }
+
+        if (target != null)
+        {
+            target.localScale = initialScale;
+        }
+
+        yield return null;
+    }
+}
