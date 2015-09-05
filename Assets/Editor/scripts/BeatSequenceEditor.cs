670202288a7462bc077eb2af77ec7620d1606ee3
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-09-05 17:09:30 +0200

    sequence editor

---
+++ Assets/Editor/scripts/BeatSequenceEditor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class BeatSequenceEditor : EditorWindow
+{
+    const float checkboxWidth = 25;
+    const float checkboxHeight = 25;
+    const float Left = 25;
+    int length;
+    Vector2 scrollPosition = Vector2.zero;
+    [MenuItem("Beats/Sequence Editor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(BeatSequenceEditor), false, "Sequence Editor");
+    }
+    void OnEnable() {
+        length = GameProgress.Sequence.data.GetLength(0);
+    }
+    void OnGUI()
+    {
+        BeatSequence b = GameProgress.Sequence;
+
+
+        EditorGUI.DrawRect(new Rect(0, 0, position.width, 30), new Color(0.3f, 0.35f, 0.4f));
+
+        float width = Left;
+        if (GUI.Button(new Rect(width, 0, 50, 20), "Revert"))
+        {
+            b.Load(BeatSequence.Path);
+        }
+        width += 55;
+        if (GUI.Button(new Rect(width, 0, 50, 20), "Save"))
+        {
+
+            b.Write(BeatSequence.Path);
+        }
+        width += 55;
+        length = EditorGUI.IntField(new Rect(width, 0, 100, 20), length);
+        width += 105;
+        if (GUI.Button(new Rect(width, 0, 20, 20), ">"))
+        {
+            b.Resize(length);
+        }
+        width += 25;
+
+        width = Left;
+
+        GUILayout.Label("", GUILayout.Width(position.width), GUILayout.Height(27));
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+        float height = 5;
+        for (int i = 1; i <= 8; i++)
+        {
+            EditorGUI.LabelField(new Rect(width, height, checkboxWidth, checkboxHeight),i+"");
+            width += checkboxWidth;
+        }
+        EditorGUI.LabelField(new Rect(width, height, checkboxWidth, checkboxHeight), "h");
+        width += checkboxWidth;
+        EditorGUI.LabelField(new Rect(width, height, checkboxWidth, checkboxHeight), "r");
+        width += checkboxWidth;
+
+        height += checkboxHeight;
+        for (int x = 0; x < b.data.GetLength(0); x++)
+        {
+            if (x % BeatSequence.MEASURELENGHT == 0)
+            {
+                EditorGUI.DrawRect(new Rect(0, height- 5, position.width, 1), new Color(0.3f, 0.35f, 0.4f));
+            }
+            width = Left;
+            for (int y = 0; y < b.data.GetLength(1); y++)
+            {
+                b.data[x, y] = EditorGUI.Toggle(new Rect(width,height,checkboxWidth, checkboxHeight), b.data[x, y]);
+                width += checkboxWidth;
+
+            }
+            height += checkboxHeight;
+        }
+        GUILayout.Label("", GUILayout.Width(position.width), GUILayout.Height(height - 30));
+        GUILayout.EndScrollView();
+    }
+}
---
+++ Assets/scripts/BeatSequence.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System;
+
+public class BeatSequence {
+    public const int MEASURELENGHT = 4;
+    public const string BARIER = "########";
+    private const char NONE = '-';
+    private const char HOLD = 'h';
+    private const char RELEASE = 'r';
+    public const string Path = "/Sequence/seq.txt";
+
+    public bool[,] data;
+    public BeatSequence(string path) {
+
+        Load(path);
+    }
+    public void Load(string path) {
+        List<bool[]> bools = new List<bool[]>();
+        string line;
+        try
+        {
+            StreamReader theReader = new StreamReader(Application.dataPath + path, Encoding.Default);
+            do
+            {
+                line = theReader.ReadLine();
+
+                if (line != null || line == BARIER)
+                {
+
+                    bools.Add(new bool[] { line[0] != NONE, line[1] != NONE, line[2] != NONE, line[3] != NONE, line[4] != NONE, line[5] != NONE, line[6] != NONE, line[7] != NONE, (line.Length > 8) ? line[8] == HOLD : false, (line.Length > 8) ? line[8] == RELEASE : false });
+
+                }
+            } while (line != null);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            data = new bool[1, 10];
+            return;
+        }
+        data = new bool[bools.Count, 10];
+        for (int x = 0; x < bools.Count; x++)
+        {
+            bool[] e = bools[x];
+            for (int y = 0; y < e.Length; y++)
+            {
+                data[x, y] = e[y];
+            }
+        }
+    }
+    public void Write(string path) {
+        StreamWriter sr = File.CreateText(Application.dataPath + path);
+        for (int x = 0; x < data.GetLength(0); x++)
+        {
+            if (x % MEASURELENGHT == 0) {
+                sr.WriteLine(BARIER);
+            }
+            string line = "";
+            for (int i = 0; i < 8; i++)
+            {
+                line += (data[x, i]) ? "X" : NONE.ToString();
+            }
+            if (data[x, 8]) {
+                line += HOLD.ToString();
+            } else if (data[x,9])
+            {
+                line += RELEASE.ToString();
+            }
+            sr.WriteLine(line);
+        }
+        sr.Close();
+    }
+    public void Resize(int length) {
+        if (length != data.GetLength(0))
+        {
+            bool[,] oldData = data;
+            data = new bool[length, 10];
+            for (int x = 0; x < data.GetLength(0) && x < oldData.GetLength(0); x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    data[x, y] = oldData[x, y];
+                }
+            }
+        }
+    }
+}
