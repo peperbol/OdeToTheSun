338ded66ab7fc5d132972e9742f24403b2608013
Linsey <linsey.92@outlook.com>
2015-09-06 11:22:51 +0200

    FIXED LE SYNC BUG~

--- Assets/scripts/StartGame.cs
+++ Assets/scripts/StartGame.cs
@@ -13,12 +13,14 @@ public class StartGame : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         bool start = true;
+		/*
         for (int i = 0; i < c.Length; i++)
         {
             start &= Input.GetKey(c[i].GetKey());
             Debug.Log(c[i].GetKey() + " " + Input.GetKey(c[i].GetKey()));
         }
         Debug.Log(start);
+*/
         if (start) {
             GameObject.FindObjectOfType<SongPlayer>().Play();
             Destroy(this);
