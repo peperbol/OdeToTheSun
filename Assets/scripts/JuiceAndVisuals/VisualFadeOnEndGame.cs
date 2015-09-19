5f622f875c4d2c3588cdb71c0e61b7bac5a37e0e
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-09-19 12:09:18 +0200

    buttons fade away on endscreen

---
+++ Assets/scripts/JuiceAndVisuals/VisualFadeOnEndGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualFadeOnEndGame : VisualFade {
+    public bool endGameVisisble;
+    public void EndGame() {
+        visible = endGameVisisble;
+    }
+}
--- Assets/scripts/Menu/EndOfGame.cs
+++ Assets/scripts/Menu/EndOfGame.cs
@@ -4,98 +4,104 @@ using System;
 
 public class EndOfGame : MonoBehaviour
 {
-  public Vector3 endPos;
-  public float endSize;
-  public float dayDelay;
-  public float nightDelay;
-  public float afterDelay;
-  public float creditFade;
-  public float zoomTime;
-  public SpriteRenderer scenery;
-  public SpriteRenderer credit;
-  public SpriteRenderer title;
-  public Sprite day;
-  public Sprite night;
-  private bool restartable = false;
-  private bool ended = false;
-  public void End()
-  {
-    if (!ended)
+    public Vector3 endPos;
+    public float endSize;
+    public float dayDelay;
+    public float nightDelay;
+    public float afterDelay;
+    public float creditFade;
+    public float zoomTime;
+    public SpriteRenderer scenery;
+    public SpriteRenderer credit;
+    public SpriteRenderer title;
+    public Sprite day;
+    public Sprite night;
+    private bool restartable = false;
+    private bool ended = false;
+    public void End()
     {
-      GameObject.FindObjectOfType<SongPlayer>().playing = false;
-      StartCoroutine(ZoomOut());
-      ended = true;
-    }
-  }
-
-  private IEnumerator ZoomOut()
-  {
-    bool finish = GameProgress.Progress > 0.75f;
-    float p = GameProgress.Progress;
-    float startSize = Camera.main.orthographicSize;
-    Vector3 startPos = Camera.main.transform.position;
-
-    scenery.sprite = (finish) ? day : night;
-    float time = 0;
-    while (time < ((finish) ? dayDelay : nightDelay))
-    {
-      time += Time.deltaTime;
-      if (!finish)
-      {
-        GameProgress.Progress = Mathf.Lerp(p, 0, time / nightDelay);
-      }
-      yield return null;
-    }
-    time = 0;
-    while (time < zoomTime)
-    {
-      time += Time.deltaTime;
-      Camera.main.transform.position = Vector3.Lerp(startPos, endPos, time / zoomTime);
-      Camera.main.orthographicSize = Mathf.Lerp(startSize, endSize, time / zoomTime);
-      yield return null;
-    }
-    time = 0;
-    while (time < afterDelay)
-    {
-
-      time += Time.deltaTime;
-      yield return null;
+        if (!ended)
+        {
+            GameObject.FindObjectOfType<SongPlayer>().playing = false;
+            StartCoroutine(ZoomOut());
+            ended = true;
+        }
     }
 
-    time = 0;
-    while (time < creditFade)
+    private IEnumerator ZoomOut()
     {
-      time += Time.deltaTime;
-      Color c = credit.color;
-      c.a = Mathf.Lerp(0, 1, time / creditFade);
-      credit.color = c;
-      title.color = c;
-      yield return null;
+        bool finish = GameProgress.Progress > 0.75f;
+        float p = GameProgress.Progress;
+        float startSize = Camera.main.orthographicSize;
+        Vector3 startPos = Camera.main.transform.position;
+
+        VisualFadeOnEndGame[] faders = GameObject.FindObjectsOfType<VisualFadeOnEndGame>();
+        for (int i = 0; i < faders.Length; i++)
+        {
+            faders[i].EndGame();
+        }
+
+        scenery.sprite = (finish) ? day : night;
+        float time = 0;
+        while (time < ((finish) ? dayDelay : nightDelay))
+        {
+            time += Time.deltaTime;
+            if (!finish)
+            {
+                GameProgress.Progress = Mathf.Lerp(p, 0, time / nightDelay);
+            }
+            yield return null;
+        }
+        time = 0;
+        while (time < zoomTime)
+        {
+            time += Time.deltaTime;
+            Camera.main.transform.position = Vector3.Lerp(startPos, endPos, time / zoomTime);
+            Camera.main.orthographicSize = Mathf.Lerp(startSize, endSize, time / zoomTime);
+            yield return null;
+        }
+        time = 0;
+        while (time < afterDelay)
+        {
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        time = 0;
+        while (time < creditFade)
+        {
+            time += Time.deltaTime;
+            Color c = credit.color;
+            c.a = Mathf.Lerp(0, 1, time / creditFade);
+            credit.color = c;
+            title.color = c;
+            yield return null;
+        }
+        restartable = true;
+        yield return null;
     }
-    restartable = true;
-    yield return null;
-  }
 
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (restartable)
+    void Start()
     {
-      SolarColor[] c = (SolarColor[])Enum.GetValues(typeof(SolarColor));
-      bool press = false;
-      for (int i = 0; i < c.Length; i++)
-      {
-        press |= Input.GetKey(c[i].GetKey());
-      }
-      if (press)
-      {
-        Application.LoadLevel(Application.loadedLevel);
-      }
+
     }
-  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (restartable)
+        {
+            SolarColor[] c = (SolarColor[])Enum.GetValues(typeof(SolarColor));
+            bool press = false;
+            for (int i = 0; i < c.Length; i++)
+            {
+                press |= Input.GetKey(c[i].GetKey());
+            }
+            if (press)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
 }
