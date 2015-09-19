45ee9eb2d3c9aa9f80788456f4ff932ecd102cb5
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-09-19 12:59:49 +0200

    added debugstart for mobile (5 second press)

--- Assets/scripts/GameFlow/StartGame.cs
+++ Assets/scripts/GameFlow/StartGame.cs
@@ -5,6 +5,7 @@ using System;
 public class StartGame : MonoBehaviour {
     SolarColor[] c;
     public bool debugSkip;
+    public bool debugTest;
     public GameObject StartVisual;
     // Use this for initialization
     void Start () {
@@ -12,7 +13,7 @@ public class StartGame : MonoBehaviour {
         c = (SolarColor[])Enum.GetValues(typeof(SolarColor));
     }
 	
-	// Update is called once per frame
+	private float debugTimer =0;
 	void Update () {
         bool start = true;
 		
@@ -20,8 +21,17 @@ public class StartGame : MonoBehaviour {
         {
             start &= InputController.GetPress((SolarColor )i);
         }
-        
-        if (start || debugSkip || Input.GetKeyDown(KeyCode.Space)) {
+
+        if (debugTest && Input.GetMouseButton(0)) {
+            debugTimer += Time.deltaTime;
+            if (debugTimer > 5) {
+                start = true;
+            }
+        }
+        else{
+            debugTimer = 0;
+        }
+        if (start || debugSkip || (debugTest && Input.GetKeyDown(KeyCode.Space))) {
             GameObject.FindObjectOfType<SongPlayer>().Play();
             Destroy(StartVisual);
             Destroy(this);
